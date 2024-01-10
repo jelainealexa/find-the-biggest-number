ecfaaa97dd09577842c59033d8a9b5a47b0005f7
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-10 17:26:08 +0800

    Pseudocode

--- find-the-biggest-number.py
+++ find-the-biggest-number.py
@@ -1,3 +1,5 @@
-#Assignment 4
+#Assignment 4: Ask user to input 3 numbers. Find and print the biggest number using only the if-else statement.
 
-#Ask user to input 3 numbers. Find and print the biggest number using only the if-else statement.
+#Ask user to input three numbers
+#Find the biggest number among the inputs using if-else statement
+#Display the biggest number
