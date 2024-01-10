4196457aa01925bbdde323e39048e63cf9638606
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-10 17:47:41 +0800

    If-Else Statement (Draft)

--- find-the-biggest-number.py
+++ find-the-biggest-number.py
@@ -6,4 +6,13 @@ second_number = float(input("Enter the second number: "))
 third_number = float(input("Enter the third number: "))
 
 #Find the biggest number among the inputs using if-else statement
+if first_number > second_number:
+    if first_number > third_number:
+      print("The biggest number is", first_number)
+elif second_number > first_number:
+    if second_number > third_number:
+        print("The biggest number is", second_number)
+else:
+    print("The biggest number is", third_number)
+
 #Display the biggest number
