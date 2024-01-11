a6b9057f57e4202dd5e5ca9c1305c9a5d1097179
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-11 10:54:16 +0800

    Labels, Entries, and Result

--- find-the-biggest-number.py
+++ find-the-biggest-number.py
@@ -9,6 +9,27 @@ window.geometry("750x750")
 window.config(bg="#000000")
 window.resizable(False, False)
 
+#Labels
+
+title_label = customtkinter.CTkLabel(window, text="Finding the Digits!")
+title_label.pack()
+
+label_first = customtkinter.CTkLabel(window, text="Enter the first number:")
+label_first.pack()
+
+label_second = customtkinter.CTkLabel(window, text="Enter the second number:", font=('Poppins', 14, 'bold'), text_color="#CC527A", bg_color="#000000")
+label_second.pack()
+
+label_third = customtkinter.CTkLabel(window, text="Enter the third number:", font=('Poppins', 14, 'bold'), text_color="#CC527A", bg_color="#000000")
+label_third.pack()
+
+#Entries
+
+#Result
+
+result_label = customtkinter.CTkLabel(window, text="")
+result_label.pack()
+
 #Ask user to input three numbers
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
@@ -32,6 +53,11 @@ def find_and_update_result():
 
         biggest_number = find_biggest_number(first_number, second_number, third_number)
 
-        print(f"The largest number is {biggest_number}!", text_color="#E8175D")
+        print(f"The largest number is {biggest_number}!")
+
+        result_label.config(text=f"The largest number is {biggest_number}!")
+
+    except ValueError:
+        result_label.config(text="Please enter valid numbers")
 
 window.mainloop()
