b893967c20f70f9f6dc10ef19d4e910673f54cb8
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-11 11:33:49 +0800

    Change geometry back to pack

--- find-the-biggest-number.py
+++ find-the-biggest-number.py
@@ -11,28 +11,27 @@ window.resizable(False, False)
 
 #Ask user to input three numbers
 
-#Labels
+#Labels and Entries
 title_label = customtkinter.CTkLabel(window, text="Finding the Digits!", font=('Poppins', 30, 'bold'), text_color="#E8175D", bg_color="#000000")
-title_label.grid(row=0, column=0, columnspan=2, pady=15)
+title_label.pack(pady=15)
 
 label_first = customtkinter.CTkLabel(window, text="Enter the first number:", font=('Poppins', 14, 'bold'), text_color="#CC527A", bg_color="#000000")
-label_first.grid(row=1, column=0, pady=5)
+label_first.pack(pady=5)
+
+enter_first = Entry(window, font=('Poppins', 14, 'bold'), width=15, justify='center', fg='#383838', bg='#E3E3E3')
+enter_first.pack()
 
 label_second = customtkinter.CTkLabel(window, text="Enter the second number:", font=('Poppins', 14, 'bold'), text_color="#CC527A", bg_color="#000000")
-label_second.grid(row=2, column=0, pady=5)
-
-label_third = customtkinter.CTkLabel(window, text="Enter the third number:", font=('Poppins', 14, 'bold'), text_color="#CC527A", bg_color="#000000")
-label_third.grid(row=3, column=0, pady=5)
-
-#Entries
-enter_first = Entry(window, font=('Poppins', 14, 'bold'), width=15, justify='center', fg='#383838', bg='#E3E3E3')
-enter_first.grid(row=1, column=1, pady=5)
+label_second.pack(pady=5)
 
 enter_second = Entry(window, font=('Poppins', 14, 'bold'), width=15, justify='center', fg='#383838', bg='#E3E3E3')
-enter_second.grid(row=2, column=1, pady=5)
+enter_second.pack()
+
+label_third = customtkinter.CTkLabel(window, text="Enter the third number:", font=('Poppins', 14, 'bold'), text_color="#CC527A", bg_color="#000000")
+label_third.pack(pady=5)
 
 enter_third = Entry(window, font=('Poppins', 14, 'bold'), width=15, justify='center', fg='#383838', bg='#E3E3E3')
-enter_third.grid(row=3, column=1, pady=5)
+enter_third.pack()
 
 #Result
 result_label = customtkinter.CTkLabel(window, text="", font=("Poppins", 25, 'bold'), text_color="#E8175D", bg_color="#000000")
@@ -68,6 +67,6 @@ def continue_button_click():
     find_and_update_result()
 
 continue_button = customtkinter.CTkButton(window, command=continue_button_click, text="Continue", corner_radius=15, cursor="hand2")
-continue_button.pack()
+continue_button.pack(pady=5)
 
 window.mainloop()
